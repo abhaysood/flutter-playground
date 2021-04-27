683db493c0a01030b0f2ceaf46600fd52d437fa6
abhay.sood <abhay.sood@gojek.com>
2021-04-27 12:47:30 +0530

    Add circular reveal

--- lib/drag_demo/drag_demo.dart
+++ lib/drag_demo/drag_demo.dart
@@ -2,6 +2,8 @@ import 'package:drag/drag_demo/task_list_item.dart';
 import 'package:drag/item.dart';
 import 'package:flutter/material.dart';
 
+import '../circular_reveal_clipper.dart';
+
 class DragDemo extends StatefulWidget {
   @override
   _DragDemoState createState() => _DragDemoState();
@@ -24,42 +26,65 @@ class _DragDemoState extends State<DragDemo> {
   }
 
   Widget _buildHeader() {
-    return Stack(
-      children: [
-        DragTarget<Task>(
-          onAccept: (data) {
-            _listGlobalKey.currentState!.removeItem(
-              _tasks.indexOf(data),
-              (_, Animation<double> animation) {
-                return SizeTransition(
-                  sizeFactor: CurvedAnimation(
-                    parent: animation,
-                    curve: Interval(0, 1),
-                  ),
-                  child: SizedBox(height: 80),
-                );
-              },
-            );
-            setState(() {
-              _tasks.remove(data);
-            });
-          },
-          builder: (_, candidateData, rejectedData) {
-            return AnimatedCrossFade(
-              crossFadeState: !_isDragging
-                  ? CrossFadeState.showFirst
-                  : CrossFadeState.showSecond,
-              duration: Duration(milliseconds: 200),
-              firstChild: _buildListHeading(),
-              secondChild: _buildDeleteDropTarget(candidateData),
+    return DragTarget<Task>(
+      onAccept: (data) {
+        _listGlobalKey.currentState!.removeItem(
+          _tasks.indexOf(data),
+          (_, Animation<double> animation) {
+            return SizeTransition(
+              sizeFactor: CurvedAnimation(
+                parent: animation,
+                curve: Interval(0, 1),
+              ),
+              child: SizedBox(height: 80),
             );
           },
-        ),
-      ],
+        );
+        setState(() {
+          _tasks.remove(data);
+        });
+      },
+      builder: (_, candidateData, rejectedData) {
+        return AnimatedSwitcher(
+          duration: Duration(milliseconds: 100),
+          child: _isDragging
+              ? _buildDropTargetHeader(candidateData)
+              : _buildListHeading(),
+        );
+      },
     );
   }
 
-  Widget _buildDeleteDropTarget(List<Task?> candidateData) {
+  Widget _buildDropTargetHeader(List<Task?> candidateData) {
+    return TweenAnimationBuilder<double>(
+      duration: Duration(milliseconds: 300),
+      tween: Tween<double>(begin: 0.1, end: 1),
+      builder: (_, value, child) {
+        return ClipPath(
+          clipper: CircularRevealClipper(
+            fraction: value,
+          ),
+          child: child,
+        );
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        alignment: Alignment.center,
+        height: 80,
+        color: candidateData.isNotEmpty
+            ? Color(0xFFFFC4BB)
+            : Color(0xFFFFC4BB).withOpacity(0.5),
+        child: Text(
+          "Drop a task here to delete",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Color(0xFF2F4858),
+          ),
+        ),
+      ),
+    );
+    /*
     return AnimatedContainer(
       duration: Duration(milliseconds: 200),
       color: candidateData.isNotEmpty
@@ -78,6 +103,7 @@ class _DragDemoState extends State<DragDemo> {
         ),
       ),
     );
+     */
   }
 
   Container _buildListHeading() {
